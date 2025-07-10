c7b9573a961b0f8dea51e4061eeb077f2d6885f5
michxlmg <jhefrigasparalmonacid0@gmail.com>
2025-07-09 19:22:52 -0500

    feat: ✨ add authentication schemas and services for registration, login, and password reset

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,21 @@
-import { axiosInstance } from "@/interceptors";
+import { axiosInstance } from "@/lib/axios";
 
-export const getProfile = async () => {
-  const response = await axiosInstance.get(
-    "http://localhost:3000/api/v1/users/profile"
-  );
+export const protectedPetition = async <T>(
+  url: string,
+  token?: string | null,
+  method: "GET" | "POST" | "PUT" | "DELETE" = "GET",
+  params?: unknown
+): Promise<T> => {
+  const response = await axiosInstance({
+    url,
+    method,
+    data: method !== "GET" ? params : undefined,
+    params: method === "GET" ? params : undefined,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return response.data;
 };
+
+export * from "./auth";
