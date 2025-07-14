ff1d29440518d9de14f02096b5603c0d09b30ffb
michxlmg <jhefrigasparalmonacid0@gmail.com>
2025-07-13 23:56:44 -0500

    feat: ✨ Add getSpaces service and Space types for space management

---
+++ src/modules/client/space/features/get_spaces/service/index.ts
@@ -0,0 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "@/interceptors/axios";
+import type { Response } from "@/types/services";
+import type { Space } from "@/modules/client/space/features/get_spaces/types";
+import { useAuth } from "@/hooks";
+
+interface SpacesResponse {
+  spaces: Space[];
+}
+
+const getSpacesRequest = async (): Promise<SpacesResponse> => {
+  const { data } = await axiosInstance.get<Response<SpacesResponse>>("/spaces");
+  if (!data.data) {
+    throw new Error("La respuesta del servidor no contiene los espacios.");
+  }
+  return data.data;
+};
+
+export const useGetSpaces = () => {
+  return useQuery({
+    queryKey: ["spaces"],
+    queryFn: getSpacesRequest,
+  });
+};
+
+export const useGetAvailableSpaces = () => {
+  const { user } = useAuth();
+  const { data: spacesData, ...rest } = useGetSpaces();
+
+  const availableSpaces = spacesData?.spaces.filter((space) => {
+    if (space.disabled) return false;
+    if (space.access === "PUBLIC") return true;
+    if (space.access === "PRIVATE") {
+      return user?.userType === "admin";
+    }
+    return true;
+  });
+
+  return {
+    ...rest,
+    data: spacesData ? { spaces: availableSpaces || [] } : undefined,
+  };
+};
