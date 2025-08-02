4de0451908e7124019b002c3af00be1e7bc2ec62
Harvey <97197014+YerikAH@users.noreply.github.com>
2025-08-02 15:16:33 -0500

    feat: :sparkles: Add some

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = `${import.meta.env.VITE_SOCKET_URL || "ws://localhost:"}${import.meta.env.VITE_PORT || "3000"}`;
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "ws://localhost:3000";
 
 export const socket = io(SOCKET_URL, {
   transports: ["websocket"],
